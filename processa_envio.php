d400216488847732e6a28c4f8d3f8becd125109d
fcintra <matheus.cintra@etec.sp.gov.br>
2021-09-13 17:09:57 -0300

    First commit

---
+++ processa_envio.php
@@ -0,0 +1,13 @@
+<?php
+    require "app_send_email/bibliotecas/PHPMailer/Exception.php";
+    require "app_send_email/bibliotecas/PHPMailer/OAuth.php";
+    require "app_send_email/bibliotecas/PHPMailer/PHPMailer.php";
+    require "app_send_email/bibliotecas/PHPMailer/POP3.php";
+    require "app_send_email/bibliotecas/PHPMailer/SMTP.php";
+
+    require "app_send_email/processa_envio.php"
+
+
+    
+
+?>
